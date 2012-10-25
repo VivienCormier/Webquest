90c7de9573cddfca09538f9406d4b0349f91d451
VivienCormier <vivien.cormier@gmail.com>
2012-10-25 23:15:21 +0200

    First Commit

---
+++ js/animation.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	
+	$('body').css('visibility','visible');
+	//controller init le plugin superscrollorama
+	var controller = $.superscrollorama();					
+	//les animations
+});
