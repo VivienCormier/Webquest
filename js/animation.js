51ae5a441e15eb42b5e1c96f8316a060bcdaab6c
Tanguy Aladenise <tanguy.aladenise@gmail.com>
2012-10-28 22:46:21 +0100

    Test d'animations et mise en forme

--- js/animation.js
+++ js/animation.js
@@ -11,10 +11,18 @@ $(document).ready(function() {
         alert("tu as cliqué sur tic");
     });
 	$("#more-tic-link").click(function(e) {
-		$("#ligne-definition").css("height", "0");
+        //alert("tu as cliqué sur more tic");
+		$("#ligne-vie-1").css("height", "0");
 		$("#texte-definition1-part1").css("width", "0");
-        alert("tu as cliqué sur more tic");
-		$("#ligne-definition").animate({height: 1252}, 1500);
-		$("#texte-definition1-part1").animate({width: 505}, 1500);
+		$("#text-association").css("height", "5");
+		
+		$("#tic-link").css('display','none');
+		
+		$("#ligne-vie-1").animate({height: 1560}, 1500);
+		$("#texte-definition1-part1").animate({width: 505}, 2500);
+		$("#text-association").delay(2500).animate({height: 503}, 2500);
+		setTimeout( function(){
+		  $("#tic-link").css('display','block');
+		},5000);
     });
 });
