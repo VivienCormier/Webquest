597099443295fd5a5bbbf1d7e385d2290f41ae3e
Tanguy Aladenise <tanguy.aladenise@gmail.com>
2012-10-28 19:38:10 +0100

    ptites modif

--- js/animation.js
+++ js/animation.js
@@ -4,4 +4,17 @@ $(document).ready(function() {
 	//controller init le plugin superscrollorama
 	var controller = $.superscrollorama();					
 	//les animations
+	
+	
+	
+	$("#tic-link").click(function(e) {
+        alert("tu as cliqué sur tic");
+    });
+	$("#more-tic-link").click(function(e) {
+		$("#ligne-definition").css("height", "0");
+		$("#texte-definition1-part1").css("width", "0");
+        alert("tu as cliqué sur more tic");
+		$("#ligne-definition").animate({height: 1252}, 1500);
+		$("#texte-definition1-part1").animate({width: 505}, 1500);
+    });
 });
